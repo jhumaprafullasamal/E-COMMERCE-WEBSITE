9b8f9c71ca4e38f13d1fb3b4ba350b3607daf322
jhumaprafullasamal <jhumaprafullasamal@gmail.com>
2026-04-17 09:17:04 +0530

    updated latest collections text

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -20,7 +20,7 @@ const LatestCollection = () => {
      <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, incidunt.
+        Discover our latest styles crafted for comfort, elegance, and everyday confidence.
         </p>
      </div>
 
